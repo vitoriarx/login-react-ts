07209e94a519d8086e55992df14c1c4b7ded1ecc
Vitoria Regina <vitoriasantos78690@gmail.com>
2023-07-31 16:01:08 -0300

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,51 @@
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  function submit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log("Email atual: ", email);
+    console.log("Senha atual:", password);
+  }
 
   return (
     <div className="login-page">
       <div id="login-card">
         <h2>Entrar</h2>
         <span>Insira suas credenciais abaixo para entrar</span>
-        <form>
+        <form onSubmit={submit}>
           <div>
-            <label htmlFor="email">Email</label><br></br>
-            <input id="email" name="email" />
+            <label htmlFor="email">Email</label>
+            <br></br>
+            <input
+              id="email"
+              name="email"
+              onChange={(event) => {
+                setEmail(event.currentTarget.value);
+              }}
+            />
           </div>
           <div>
-            <label htmlFor="senha">Senha</label><br></br>
-            <input id="senha" name="senha" />
+            <label htmlFor="senha">Senha</label>
+            <br></br>
+            <input
+              id="senha"
+              name="password"
+              type="password"
+              onChange={(event) => {
+                setPassword(event.currentTarget.value);
+              }}
+            />
           </div>
           <button type="submit">Entrar</button>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom/client';
-import App from './app/App';
-import './globals.css';
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import "./globals.css";
 
-const divRoot = document.getElementById('root') as HTMLElement;
+const divRoot = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(divRoot).render(<App />);
